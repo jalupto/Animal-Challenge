e6793d1f77c22fe3fb63106fed024909dddfaf27
Jared Upton <jalupto@gmail.com>
2021-06-15 21:11:25 -0400

    CRUD solutions for animal server

--- controllers/animalcontroller.js
+++ controllers/animalcontroller.js
@@ -18,8 +18,66 @@ router.post("/create", async(req, res) => {
     // Animal.create(animalEntry)
 });
 
-router.get('/list', (req, res) => {
-    res.send('This is the list of animals.')
+router.get('/', async(req, res) => {
+    try {
+        const allAnimals = await Animal.findAll();
+        res.status(200).json(
+            allAnimals
+        )
+    } catch (error) {
+        res.status(500).json({
+            error: `You have an error: ${error}`
+        })
+    }
+});
+
+router.delete('/delete/:name', async(req, res) => {
+    const animalToDelete = req.params.name;
+    try {
+        const query = {
+            where: {
+                name: animalToDelete
+            }
+        };
+
+        await Animal.destroy(query);
+        res.status(200).json({
+            message: `The animal ${animalToDelete} has been deleted.`
+        });
+    } catch (error) {
+        res.status(500).json({
+            message: `There was an issue deleting this animal: ${error}`,
+            error,
+        });
+    }
+});
+
+router.put('/update/:id', async(req, res) => {
+    const { name, legNumber, predator } = req.body.animal;
+
+    const query = {
+        where: {
+            id: req.params.id
+        }
+    }
+
+    const updatedAnimal = {
+        name: name,
+        legNumber: legNumber,
+        predator: predator
+    }
+
+    try {
+        const update = await Animal.update(updatedAnimal, query);
+        res.status(200).json({
+            message: 'Animal evolved!',
+            update
+        })
+    } catch (err) {
+        res.status(500).json({
+            message: 'Something went wrong!'
+        });
+    }
 });
 
 module.exports = router;
